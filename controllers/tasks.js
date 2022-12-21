c8e23dd51947a5cf3ec899eeeddc62e41c4ee155
othmaneuw <strangeothmane@gmail.com>
2022-12-21 07:06:37 +0100

    async wrapper

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,39 +1,30 @@
 const Task = require('../models/Task');
+const asyncWrapper = require('../middleware/async');
 
 //GETTING ALL TASKS
-const getAllTasks = async (req,res) =>{
-    try{
+const getAllTasks = asyncWrapper (async (req,res) =>{
         const tasks = await Task.find();
         res.status(200).json({ tasks });
-    }catch(err){
-        res.status(500).json({err})
-    }
-}
+});
 
-const createTask = async (req,res) =>{
-    try{
+const createTask = asyncWrapper( async (req,res) =>{
         const task = await Task.create(req.body);
         res.status(201).json({task});
-    }catch(err){
-        res.status(500).json({message : err});
-    }
-}
+})
 
-const getOneTask = async (req,res) =>{
-    try{
+const getOneTask = asyncWrapper (async (req,res) =>{
+
         const {id:taskID} = req.params;
         const task = await Task.findOne({_id : taskID});
         if(!task) return res.status(404).json({mssg : "Task not Found"});
         res.status(200).json({task});
-    }catch(err){
-        res.status(500).json({err});
-    }
-}
+   
+})
 
-const updateTask = async (req,res) =>{
+const updateTask = asyncWrapper( async (req,res) =>{
     const {id:taskID} = req.params;
-    try{
-        const task = await Task.findByOneAndUpdate({_id : taskID},req.body,{
+
+        const task = await Task.findOneAndUpdate({_id : taskID},req.body,{
             new : true,
             runValidators : true
         });
@@ -41,21 +32,14 @@ const updateTask = async (req,res) =>{
 
         res.status(200).json({taskID , data:task})
 
-    }catch(err){
-        res.status(500).json({err});
-    }
-}
+})
 
-const deleteTask = async (req,res) =>{
-    try{
+const deleteTask = asyncWrapper( async (req,res) =>{
         const {id : taskID} = req.params;
         const task = await Task.findOneAndDelete({_id : taskID});
         if(!task) return res.status(404).json({mssg : 'No task Found'});
         res.status(200).json({task});
-    }catch(err){
-         res.status(500).json({err});
-    }
-}
+})
 
 module.exports = {
     getAllTasks,
